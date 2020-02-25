bd6ffb232122f7c55d0fd33c7fc3b75fc5996a7a
it-206a <it-206a@IT-206A-22>
2020-02-25 15:31:29 +0530

    first commit

---
+++ log/src/main/java/construction/Con.java
@@ -0,0 +1,12 @@
+package construction;
+
+import java.io.*;
+
+public class Con {
+	public static void main(String[] args)  {
+	
+		Cost c = new Cost();
+        c.con();
+	}
+
+}
---
+++ log/src/main/java/task5/log/Sample.java
@@ -0,0 +1,13 @@
+package task5.log;
+import java.io.*;
+public class Sample
+{ 
+	 
+ public static void main(String args[]) 
+ {
+	 System.out.println("");
+	 Interest obj=new Interest();
+	 obj.sim();
+ }
+
+} 
